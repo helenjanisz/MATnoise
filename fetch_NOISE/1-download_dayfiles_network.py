866a90de2cc7eb424c14e5dcd685942b294f4217
noahkenjohnson-wq <noah.ken.johnson@gmail.com>
2026-06-04 09:24:21 -1000

    Change data collection date

--- fetch_NOISE/1-download_dayfiles_network.py
+++ fetch_NOISE/1-download_dayfiles_network.py
@@ -31,14 +31,14 @@ import pandas as pd
 # %% codecell
 webservice = "IRIS"
 networks = ['XO'] # YO ENAM; ZA NoMelt
-tstart = "2018-09-00T00:00:00"
-tend = "2018-10-00T00:00:00"
+tstart = "2018-09-01T00:00:00"
+tend = "2018-10-01T00:00:00"
 is_downsamp = 1 # downsample data?
 sr_new = 50 # Hz New sample rate
 trlen = 24*60*60 # s
 # WARNING! List the full channel names. Do not use wildcards. Bad things will happen...
 comps = ['HHZ','HH1','HH2','HHN','HHE','LHE','LHN','LHZ','LH1','LH2','EHN','EHE','EHZ'] #["HXZ", "HX1", "HX2"] #["LHZ", "LH1", "LH2"] #["HHZ", "HH1", "HH2"] #["HHZ", "HH1", "HH2", "BDH"]
-homedir = "path/to/output/data/directory/" # "./"
+homedir = "/Users/noah/AACES Data 09-10:2018" # "./"
 is_removeresp = 1 # Remove response?
 outunits = 'DISP' # DISP, VEL, ACC [For pressure channels, should use "VEL"]
 is_overwrite = 0 # overwrite ? 
