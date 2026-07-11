74c016d258be2a879e5609c9506ce09d4a4fccd5
noahkenjohnson-wq <noah.ken.johnson@gmail.com>
2026-07-10 14:33:43 -1000

    not needed

--- untitled.m
+++
@@ -1,9 +0,0 @@
-function cleanLargeVars(thresholdBytes)
-    vars = whos;
-    
-    for i = 1:length(vars)
-        if vars(i).bytes > thresholdBytes
-            eval(['clear ' vars(i).name])
-        end
-    end
-end
